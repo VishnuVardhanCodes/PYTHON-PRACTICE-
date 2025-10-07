32f5b534b6e42a5d4dd936464a69a36e1903cd07
VishnuVardhanCodes <vishnumaxpolla32@email.com>
2025-10-07 21:44:36 +0530

    Initial commit - Python Basics practice

---
+++ basics.py
@@ -0,0 +1,135 @@
+print("Hello World")
+print("Vishnu Vardhan")
+
+#variables - variables are the placeholders for the storing data.  
+# x- variable name , = assignment operator , 10 value to be assigned 
+
+a= 5
+print(a)
+
+x= 5 
+id(x)
+
+#this is the comment 
+print("Im Learning Python ")#this is also the comment 
+
+#CREATING VARIABLE
+x= "vishnu "
+y= "vardhan"
+print(x)
+print(y)
+print(x+y)
+
+x=10
+y=20
+z=30
+print(y)
+print(z)
+print(y)
+
+x="vishnu"
+x="vardhan"
+print(x)
+
+#CASTING 
+x= str(3) #string value it will give here
+y= float(3) #it gives the float values like decimal in points 
+z=int(3) # it gives hte number integer values only
+print(y)
+print(z)
+print(y)
+
+# CHEKCKING THE TYPE OF THE VARIABLE
+x= "vishnu "
+y=20
+z=3.0
+print(type(x))
+print(type(y))
+print(type(z))
+
+# ASSIGN MULTIPLE VALUES
+x,y,z="vishnu","vardhan","polla"
+print(x)
+print(y)
+print(z)
+
+ #  ASSIGN ONE VALUE TO MULTIPLE VALUES
+x=y=z="VISHNU"
+print(x,y,z)
+
+# TYPES OF DATA TYPES 
+#INTEGER 
+x= 20
+print(type(x))
+print(x)
+
+#float
+x= 2.0
+print(type(x))
+print(x)
+
+#boolean
+x= True
+print(type(x))
+print(x)
+
+#NONE
+x= None
+print(type(x))
+print(x)
+
+#STRING
+x= "VISHNU VARDHAN POLLA"
+print(type(x))
+print(x)
+
+a="vishnu"
+print(a)
+
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# F SRTING
+name = "vishnu"
+age = 21
+txt= f"my name is {name},my age is {age}"
+print(txt)
+
+
+color="green"
+number="4"
+txt=f"my fav color is {color}, my fav number is {number}"
+print(txt)
+
+#BOOLEAN
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#PYTHON OPERATOR 
+print(5+10)
+print(5-10)
+print(5*10)
+print(5/10)
+print(5%10)
+
+x=30
+y=20
+z=10
+print(x+y+z)
+print(x-y-z)
+print(x*y*z)
+
+a=20
+b=40
+print(a==b)
+print(a!=b)
+
+
+
+
+
+
